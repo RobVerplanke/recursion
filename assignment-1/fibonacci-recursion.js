73ceb3ff8f84d94bf2785c7acd8e7b8dc31931e5
RobVerplanke <rverplanke@gmail.com>
2023-09-06 15:05:33 +0200

    make recursive function

--- assignment-1/fibonacci-recursion.js
+++ assignment-1/fibonacci-recursion.js
@@ -1,9 +1,12 @@
-function fibsRec(number){
-    let result = [0,1];
+function fibsRec(n) {
 
-    // base case
-
-    // recursive call
-
-    return result;
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+    
+    const prevFibs = fibsRec(n - 1);
+    const nextFib = prevFibs[prevFibs.length - 1] + prevFibs[prevFibs.length - 2];
+    return [...prevFibs, nextFib];
 }
+
+console.log(fibsRec(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
