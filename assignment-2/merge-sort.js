d7d4e012beace991f420358168a2b4bd83d8bd29
RobVerplanke <rverplanke@gmail.com>
2023-09-11 14:47:19 +0200

    complete sortMerge function

--- assignment-2/merge-sort.js
+++ assignment-2/merge-sort.js
@@ -1,18 +1,41 @@
 function mergeSort(array){
-
+    
+    if (array.length === 1) return array; // base case
+    
     let middleIndex = Math.floor(array.length / 2);
     let leftSide = array.slice(0, middleIndex);
-    let rightSide = array.slice(middleIndex);
-    let sortedArray = [];
-
-    if (array.length === 1) return array; // base case
+    let rightSide = array.slice(middleIndex, array.length);
 
     // recursive call
-    mergeSort(leftSide);
-    mergeSort(rightSide);
+    return merge(mergeSort(leftSide), mergeSort(rightSide));
+}
 
-    // sort
+// sort and merge
+function merge(left, right){
+    let sortedArray = [];
+    
+    while(left.length > 0 && right.length > 0){
+       
+        if(left[0] < right[0]){
+            sortedArray.push(left[0]);
+            left.shift();
+        } else{
+            sortedArray.push(right[0]);
+            right.shift();
+        }
+    }
 
+    while (right.length > 0) {
+        sortedArray.push(right[0]);
+        right.shift();
+    }
+
+    while (left.length > 0) {
+        sortedArray.push(left[0]);
+        left.shift();
+    }
+
+    return sortedArray;
 }
 
 const testArray = [7,26,4,11,8,2,10,14,21,6,15,3,18,12,5,29];
